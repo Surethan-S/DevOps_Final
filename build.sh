6f19a43b0076ae309311ba3760fa058f91749712
Surethan-S <127739936+Surethan-S@users.noreply.github.com>
2025-04-03 21:14:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-docker build -t task7 .
-echo Hyyy
-docker login -u surethan37 -p 55665566@S37
-docker tag task7 surethan37/final_task
-docker push surethan37/final_task
-kubectl apply -f  deploy.yaml --validate=false
-kubectl apply -f svc.yaml --validate=false
+
+echo "Building React App..."
+npm install
+npm run build
+echo "React App Build Completed!"
