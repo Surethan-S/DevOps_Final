aa4a1122e40273195cf19e77ec1c55eefb2f193e
Surethan-S <127739936+Surethan-S@users.noreply.github.com>
2025-04-03 21:29:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "Building React App..."
-npm install
-npm run build
+
+cd build  # Change into the build directory
+npm install  # Install dependencies
+npm run build  # Run the build command
+
 echo "React App Build Completed!"
