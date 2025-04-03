1776ffec5044076063369f5ea6a316f3bf021d3a
Surethan-S <127739936+Surethan-S@users.noreply.github.com>
2025-04-03 20:43:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker build -t task37 .
+docker build -t task7 .
 echo Hyyy
 docker login -u surethan37 -p 55665566@S37
-docker tag task37 surethan37/task2
-docker push surethan37/task2
+docker tag task7 surethan37/final_task
+docker push surethan37/final_task
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
